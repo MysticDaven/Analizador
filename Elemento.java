f893f67f8156060f342a1cf17180404353cd5620
AngekGP <joseangel7272@hotmail.com>
2022-10-31 22:21:37 -0600

    class fixed

--- Elemento.java
+++ Elemento.java
@@ -1,48 +1,54 @@
-public static class Elemento{
+public class Elemento {
     String id;
     String categoria;
     String tipo;
     String size;
+
+    public Elemento() {
+    }
+
+    public Elemento(String id, String tipo) {
+        this.id = id;
+        this.tipo = tipo;
+    }
+
+    public Elemento(String id, String categoria, String tipo, String size) {
+        this.id = id;
+        this.categoria = categoria;
+        this.tipo = tipo;
+        this.size = size;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSize() {
+        return this.size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
 }
-
-public Elemento(){}
-
-public Elemento(String id, String categoria, String tipo, String size){
-    this.id = id;
-    this.categoria = categoria;
-    this.tipo = tipo;
-    this.size = size;
-}
-
-public String getId(){
-    return this.id;
-}
-
-public void setId(String id){
-    this.id = id;
-}
-
-public String getCategoria(){
-    return this.categoria;
-}
-
-public void setCategoria(String categoria){
-    this.categoria = categoria;
-}
-public String getTipo(){
-    return this.tipo;
-}
-
-public void setTipo(String tipo){
-    this.tipo = tipo;
-}
-
-public String getSize(){
-    return this.size;
-}
-
-public void setSize(String size){
-    this.size = size;
-}
-
-
